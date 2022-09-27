a4d19ec7feeb7e4855fb888544cc65198daee129
hgergo <gergo.hornak@adnovum.hu>
2022-09-27 11:10:15 +0200

    refactor

--- src/main/java/guru/springframework/repositories/UnitOfMesureRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.UnitOfMesure;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UnitOfMesureRepository extends CrudRepository<UnitOfMesure, Long> {
-}
